8fc0ca81540b01d8e5372d1d0edae6630fc1d4f5
Genialqua <fred.aleokemalachi@gmail.com>
2022-06-06 21:22:31 +0000

    Printing without the printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,17 @@
 #!/bin/bash
-gcc int puts(const char *Programming is like building a multilingual puzzle\n)
+#include <stdio.h>
+#include <string.h>
+
+/*To print without using printf*/
+int main(0)
+{
+	char str1[44];
+
+	strcpy(str1,"Programming is like building a multilingual puzzle\n");
+
+	puts(str1);
+
+	return(0)
+}
+
+
