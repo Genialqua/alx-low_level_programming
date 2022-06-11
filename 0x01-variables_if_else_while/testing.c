b85de4fe297672f3a3666b181d6b22290c148dcb
Genialqua <fred.aleokemalachi@gmail.com>
2022-06-11 15:09:06 +0000

    To assign polarity to random numbers

---
+++ 0x01-variables_if_else_while/testing.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void)
+{
+	/*This would evaluate char size*/
+
+	 *int a;
+	 *int b;
+	 *
+	 *a = 2;
+	 *b = 1;
+	 *
+	 *if
+	 (a >= b);
+
+	 *printf("%d", 2 == 1);
+	 *(return 0);
+}
