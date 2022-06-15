53ee56aa66a53343df3fcd1622b234b8795be9f9
Genialqua <fred.aleokemalachi@gmail.com>
2022-06-15 20:28:54 +0000

    A function that checks for alphabetic characters

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _isalpha - Prints alphabets
+ * @c: Description
+ * Return: Always return 0
+ */
+
+int _isalpha(int c)
+{
+	int isalpha(int c);
+
+	if (isalpha(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
