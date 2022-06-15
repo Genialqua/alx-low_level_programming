cc5d83493f549935dc16d5345c4f8808e1dbfef6
Genialqua <fred.aleokemalachi@gmail.com>
2022-06-15 18:38:17 +0000

    A function that prints putchar

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -11,10 +11,7 @@ void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
-# cat main.h
-#ifndef MAIN_H
 #define MAIN_H
-int _putchar(char c);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
