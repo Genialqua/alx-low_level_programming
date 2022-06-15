d5deaa574dcce0c65b74e6c0828ecaff767e8a05
Genialqua <fred.aleokemalachi@gmail.com>
2022-06-15 19:35:49 +0000

    A function that prints 10times the lowercase alphabets

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Check code
+ *
+ * Return: always return 0 (success)
+ */
+
+void print_alphabet_x10(void)
+{
+	int print_alphabet_x10 = 1;
+	char ch;
+
+	while (print_alphabet_x10 <= 10)
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+{
+	_putchar(ch);
+}
+	_putchar('\n');
+	print_alphabet_x10++;
+}
