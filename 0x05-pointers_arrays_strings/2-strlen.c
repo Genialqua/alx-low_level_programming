6cad5d322f6e1d324695abebf0730dd06d063602
Genialqua <fred.aleokemalachi@gmail.com>
2022-06-15 15:49:19 +0000

    To display the length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strlen - A function to return the length of a string
+ * @s: char input
+ * Return: length of the input string
+ */
+
+int _strlen(char *s)
+{
+	int i = 1, sum = 0;
+	char p1 = s[0];
+
+	while (p1 != '\0')
+	{
+		sum++;
+		p1 = s[i++];
+	}
+	return (sum);
+
+}
