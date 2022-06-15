12d605887b96fa8999f7af435318ca94896e2aad
Genialqua <fred.aleokemalachi@gmail.com>
2022-06-15 20:06:12 +0000

    A function that checks lowercase character

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _islower - checks code in lowercase
+ *
+ * @c: basics
+ * Return: Always 0
+ */
+
+int _islower(int c)
+{
+	int islower(int c);
+
+	if (islower(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
