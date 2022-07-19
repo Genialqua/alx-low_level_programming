b60ec52e01d1d7f8db58a46a2871aa9e65df13fa
Genialqua <fred.aleokemalachi@gmail.com>
2022-07-19 20:46:28 +0000

    A function that returns the number of elements in a linked list.

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -6,15 +6,16 @@
  *
  * Return: numbers of nodes.
  */
+
 size_t print_listint(const listint_t *h)
 {
-	size_t nnodes = 0;
+	size_t len = 0;
 
 	while (h != NULL)
 	{
 	printf("%d\n", h->n);
+	len++;
 	h = h->next;
-	nnodes++;
 	}
-	return (nnodes);
+	return (len);
 }
