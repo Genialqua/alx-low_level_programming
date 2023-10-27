be87261a40741fef5026be87e3aeba1ba9428bce
Frederick Aleoke-Malachi <fred.aleokemalachi@gmail.com>
2023-10-27 20:52:26 +0100

    A script to print a string with newline

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point into the program
+ * Description: 'This program prints to stdout'
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char mystr[] = "\"Programming is like building a multilingual puzzle";
+
+puts(mystr);
+return (0);
+}
