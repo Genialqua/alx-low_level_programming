7ac95e830c262d872eefa62d94735b9e1604a894
Frederick Aleoke-Malachi <fred.aleokemalachi@gmail.com>
2023-10-27 22:08:23 +0100

    To print a string using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: 'This program print to STDout using printf'
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char mystr[] = "with proper grammar, but the outcome is a piece of art,\n";
+printf("%s",mystr);
+return (0);
+}
