46fe31a835837da777a3a482b17c4f68c16e0d11
Frederick Aleoke-Malachi <fred.aleokemalachi@gmail.com>
2023-11-23 13:12:26 +0100

    A program to print numbers using putchar.

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point into the program.
+ * Description: 'This code prints numbers.'
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+int i;
+for (i = 48; i < 58; ++i)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
