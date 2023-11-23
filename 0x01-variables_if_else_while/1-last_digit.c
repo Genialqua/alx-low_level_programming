70812930925f2cf3fa79d9c3f268cfdda3b1c079
Frederick Aleoke-Malachi <fred.aleokemalachi@gmail.com>
2023-11-23 07:40:03 +0100

    A program to determine the last digit of a number

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -23,7 +23,7 @@ if (last_digit > 5)
 printf("Last digit of %d is %d and is greater than 5\n", n, last_digit);
 }
 
-else if ((last_digit > 0) && (last_digit < 6))
+else if ((last_digit < 0) && (last_digit < 6))
 {
 printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_digit);
 }
