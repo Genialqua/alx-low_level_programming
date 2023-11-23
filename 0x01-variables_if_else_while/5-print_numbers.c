4dd133065f7c606dd7e51cf22ce1a6b0ebf4b853
Frederick Aleoke-Malachi <fred.aleokemalachi@gmail.com>
2023-11-23 12:46:38 +0100

    A program to print numbers.

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point into the program.
+ * Description: 'This code prints numbers.'
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+int i = 0;
+for (i = 0; i < 10; ++i)
+{
+printf("%d", i);
+}
+putchar('\n');
+return (0);
+}
