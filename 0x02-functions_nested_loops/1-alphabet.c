880e426238ceef27a53dcb482d5ab8a30910f250
Frederick Aleoke-Malachi <fred.aleokemalachi@gmail.com>
2023-12-01 14:06:08 +0100

    Created a function to print alphabets

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,18 +2,18 @@
 
 /**
  * print_alphabet - Prints alphabets in lowercase.
- * 
+ *
  * Return: Always 0 (success)
 */
 
 void print_alphabet(void)
 {
-    char i = 'a';
-    while (i <= 'z')
-    {
-         _putchar(i);
-         i++;
-    }
-    _putchar('\n');
-   
+char i = 'a';
+
+while (i <= 'z')
+{
+_putchar(i);
+i++;
+}
+_putchar('\n');
 }
