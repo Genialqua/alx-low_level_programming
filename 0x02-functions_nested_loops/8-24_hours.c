fa3e72d21a46d5fb195364b08529efca3d087d50
Frederick Aleoke-Malachi <fred.aleokemalachi@gmail.com>
2023-12-03 16:36:35 +0100

    A function that prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59.

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * jack_bauer - A program that checks for
+ * sign on numbers
+ *
+ * Return: void
+ */
+
+void jack_bauer(void)
+{
+int i;
+int j;
+int k;
+int l;
+
+for (l = 0; l <= 2; l++)
+{
+for (k = 0; k <= 3; k++)
+{
+for (j = 0; j <= 5; j++)
+{
+for (i = 0; i <= 9; i++)
+{
+_putchar('0' + l);
+_putchar('0' + k);
+_putchar(':');
+_putchar('0' + j);
+_putchar('0' + i);
+_putchar('\n');
+}
+}
+}
+}
+}
